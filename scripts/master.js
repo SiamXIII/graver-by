f7459eee3240d47f48550b5968f635bbfcf30e89
Aliaksandr Lindarenka <siamxiii@gmail.com>
2015-04-10 18:51:03 +0300

    trans

---
+++ scripts/master.js
@@ -0,0 +1,39 @@
+﻿jQuery(document).ready(function ($) {
+	//main slider
+	var _CaptionTransitions = [{
+		$Duration: 900,
+		$Clip: 1,
+		$Move: true,
+		$Easing: {
+			$Clip: $JssorEasing$.$EaseInOutCubic
+		}
+	}];
+
+	var options = {
+		$AutoPlay: true,
+		$BulletNavigatorOptions: {
+			$Class: $JssorBulletNavigator$,
+			$ChanceToShow: 2,
+			$SpacingX: 10,
+			$AutoCenter: 1
+		},
+		$CaptionSliderOptions: {
+			$Class: $JssorCaptionSlider$,
+			$CaptionTransitions: _CaptionTransitions,
+			$PlayInMode: 1,
+			$PlayOutMode: 3
+		}
+	};
+	var mainSlider = new $JssorSlider$('mainSlider', options);
+})
+
+//catalog
+$('#catalog').slimmenu(
+{
+	resizeWidth: '1200',
+	collapserTitle: 'Main Menu',
+	animSpeed: 'medium',
+	easingEffect: null,
+	indentChildren: false,
+	childrenIndenter: '&nbsp;'
+});
