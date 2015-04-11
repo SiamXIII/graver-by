17c26d3c0e4c0f749b59e47f0273d862e26b79c6
Siam_Zero <siamxiii@gmail.com>
2015-04-11 17:19:08 +0300

    added popular

--- scripts/master.js
+++ scripts/master.js
@@ -1,39 +1,28 @@
 ﻿jQuery(document).ready(function ($) {
-	//main slider
-	var _CaptionTransitions = [{
-		$Duration: 900,
-		$Clip: 1,
-		$Move: true,
-		$Easing: {
-			$Clip: $JssorEasing$.$EaseInOutCubic
-		}
-	}];
+    //main slider
+    var options = {
+        $AutoPlay: true,
+        $BulletNavigatorOptions: {
+            $Class: $JssorBulletNavigator$,
+            $ChanceToShow: 2,
+            $SpacingX: 10,
+            $AutoCenter: 1
+        }
+    };
+    var mainSlider = new $JssorSlider$('mainSlider', options);
 
-	var options = {
-		$AutoPlay: true,
-		$BulletNavigatorOptions: {
-			$Class: $JssorBulletNavigator$,
-			$ChanceToShow: 2,
-			$SpacingX: 10,
-			$AutoCenter: 1
-		},
-		$CaptionSliderOptions: {
-			$Class: $JssorCaptionSlider$,
-			$CaptionTransitions: _CaptionTransitions,
-			$PlayInMode: 1,
-			$PlayOutMode: 3
-		}
-	};
-	var mainSlider = new $JssorSlider$('mainSlider', options);
+    //popular slider
+    var popularOptions = {
+        $AutoPlay: false,
+        $DragOrientation: 2,
+        $PlayOrientation: 2,
+        $ArrowNavigatorOptions: {                       //[Optional] Options to specify and enable arrow navigator or not
+            $Class: $JssorArrowNavigator$,              //[Requried] Class to create arrow navigator instance
+            $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+            $AutoCenter: 0,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+            $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
+        }
+    };
+    var mainSlider = new $JssorSlider$('popularSlider', popularOptions);
 })
 
-//catalog
-$('#catalog').slimmenu(
-{
-	resizeWidth: '1200',
-	collapserTitle: 'Main Menu',
-	animSpeed: 'medium',
-	easingEffect: null,
-	indentChildren: false,
-	childrenIndenter: '&nbsp;'
-});
