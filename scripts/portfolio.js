b1214c5b5482d743a2a75bcfd6688382068f39c0
Aliaksandr Lindarenka <siamxiii@gmail.com>
2015-04-16 14:08:13 +0300

    portfolio

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,6 +1,8 @@
 ﻿jQuery(document).ready(function ($) {
 	var instagramUrl = 'https://api.instagram.com/v1/users/732051385/media/recent/?access_token=732051385.674061d.3cab369edb5147adbf8a8285f70c423e&count=60 ';
 
+	var selectedImage = 0;
+
 	function getNext(instaUrl) {
 		$.ajax({
 			url: instaUrl,
@@ -9,7 +11,11 @@
 			success: function appendInsta(data, status, jqXHR) {
 				if (data) {
 					$.each(data.data, function (index, value) {
-						$('#instagram').append('<li class="tile"><img src="' + value.images.low_resolution.url + '" data-full="' + value.images.standard_resolution.url + '"/></li>');
+						$('#instagram').append('<li class="tile"><img src="' + value.images.low_resolution.url
+							+ '" data-full="' + value.images.standard_resolution.url
+							+ '" data-type="' + value.type
+							+ '" data-link="' + value.link
+							+ '" data-tags="' + createTags(value.tags) + '" /></li>');
 					})
 
 					if (data.pagination.next_url) {
@@ -17,9 +23,18 @@
 					}
 					else {
 						$('#instagram li.tile img').click(function () {
-							$('#fullImage').attr('src', this.attributes['data-full'].value)
-							$('#fullImageContainer').show('fast');
+							selectedImage = this;
+							if (this.attributes['data-type'].value == 'image') {
+								settFullImage(this);
+							}
+							if (this.attributes['data-type'].value == 'video') {
+								window.open(this.attributes['data-link'].value, 'blank');
+							}
 						});
+
+						$('#fullImageContainer .right.arrow').click(getNextImage);
+
+						$('#fullImageContainer .left.arrow').click(getPrevImage);
 
 						$('#fullImageContainer .underlay').click(function () {
 							$('#fullImageContainer').hide('fast');
@@ -35,6 +50,37 @@
 				}
 			}
 		});
+	}
+
+	function createTags(tags) {
+		var hashtags = '';
+		$.each(tags, function (index, value) {
+			hashtags += '#' + value + ' ';
+		})
+
+		return hashtags;
+	}
+
+	function settFullImage(image) {
+		var currentImage = new Image();
+		currentImage.src = image.attributes['data-full'].value;
+		currentImage.id = 'fullImage';
+		$('#description').text(image.attributes['data-tags'].value);
+
+		currentImage.onload = function () {
+			$('#fullImage').replaceWith(currentImage);
+			$('#fullImageContainer').show('fast');
+		};
+	}
+
+	function getNextImage() {
+		selectedImage = $(selectedImage).parent('.tile').next('.tile').children('img')[0];
+		settFullImage(selectedImage);
+	}
+
+	function getPrevImage() {
+		selectedImage = $(selectedImage).parent('.tile').prev('.tile').children('img')[0];
+		settFullImage(selectedImage);
 	}
 
 	getNext(instagramUrl);
